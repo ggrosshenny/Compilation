f5d3c07ac5642b50d59ef0b843733cbaf01bc80f
Guillaume.Grosshenny <guillaume.grosshenny@etu.unistra.fr>
2017-11-20 14:28:12 +0100

    Tests for the symbol table. Compiles and tests pass.

--- AST/test_symTable.c
+++ AST/test_symTable.c
@@ -1,23 +1,43 @@
 #include "symbolTable.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 
 int main()
 {
 
   symTable* test = symTable_init();
 
-  symTable_newTemp(test, 42);
+  symbol* testSymbol = NULL;
 
-  symTable_addConst(test, "lol", 17);
+  testSymbol = symTable_newTemp(test, 42);
+  assert(strcmp(testSymbol->identifier, "$temp_0")==0);
+  assert(testSymbol->isConstant == false);
+  assert(testSymbol->value == 42);
 
-  symTable_newTemp(test, 50);
+  testSymbol = symTable_addConst(test, "lol", 17);
+  assert(strcmp(testSymbol->identifier, "lol")==0);
+  assert(testSymbol->isConstant == true);
+  assert(testSymbol->value == 17);
 
-  symTable_newTemp(test, 67);
+  testSymbol = symTable_newTemp(test, 50);
+  assert(strcmp(testSymbol->identifier, "$temp_1")==0);
+  assert(testSymbol->isConstant == false);
+  assert(testSymbol->value == 50);
 
-  symTable_addConst(test, "const", 54);
+  testSymbol = symTable_newTemp(test, 67);
+  assert(strcmp(testSymbol->identifier, "$temp_2")==0);
+  assert(testSymbol->isConstant == false);
+  assert(testSymbol->value == 67);
 
-  symTable_addConst(test, "const", 72);
+  testSymbol = symTable_addConst(test, "const", 54);
+  assert(strcmp(testSymbol->identifier, "const")==0);
+  assert(testSymbol->isConstant == true);
+  assert(testSymbol->value == 54);
+
+  testSymbol = symTable_addConst(test, "const", 72);
+  assert(testSymbol == NULL);
 
   symTable_print(test);
 
